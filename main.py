ffe7ebbe174f36e1ae472fbe12e003798e257e05
matthew <mhillis57@gmail.com>
2025-06-10 21:50:35 -0500

    Added player.py and circleshape.py, a triangle is drawn to the screen and can move around with WASD input from user

--- main.py
+++ main.py
@@ -3,19 +3,31 @@
 # throughout this file
 import pygame
 from constants import *
-
+from player import *
 def main():
 	pygame.init()
+	#GROUPS AND CONTAINERS
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
+	#
+	clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0,0,0))
+		updatable.update(dt)
+		for drawables in drawable:
+			drawables.draw(screen)
 		pygame.display.flip()
-		#print("Starting Asteroids!")
-		#print("Screen width: " + str(SCREEN_WIDTH))
-		#print("Screen height: " + str(SCREEN_HEIGHT))
+
+		dt = (clock.tick(60) / 1000)
 
 if __name__=="__main__":
 	main()
