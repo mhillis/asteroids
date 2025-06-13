2d441a5a6f0f8c0e5e41342caeca0aff034fbec7
matthew <mhillis57@gmail.com>
2025-06-12 21:23:44 -0500

    Added split function to asteroid so now asteroids split when shot and only disapear if they are small

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,7 @@
-from circleshape import *
+from circleshape import CircleShape
+from  constants import *
+import random
+import pygame
 
 class Asteroid(CircleShape):
 	def __init__(self, x, y, radius):
@@ -10,3 +13,19 @@ class Asteroid(CircleShape):
 	def update(self,dt):
 		self.position += self.velocity * dt
 
+	def split(self):
+		self.kill()
+
+		if self.radius <= ASTEROID_MIN_RADIUS:
+			return
+
+		random_angle = random.uniform(20,50)
+
+		first_vector = self.velocity.rotate(random_angle)
+		second_vector = self.velocity.rotate(-random_angle)
+
+		new_radius = self.radius - ASTEROID_MIN_RADIUS
+		asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+		asteroid1.velocity = first_vector * 1.2
+		asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+		asteroid2.velocity = second_vector * 1.2
